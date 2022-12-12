adc77fd64425d83a040ba1879bc95b5a6c6abdf9
Tae Yeong Kim <taeyeong200@gmail.com>
2022-12-13 00:24:13 +1100

    testing

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import discord
+import discord, os
 
-client = discord.Client()
+client = discord.Client(intents=discord.Intents.default())
+token = os.environ['DISCORD_BOT_TOKEN']
 
 @client.event
 async def on_message(message):
     if message.content.startswith('!hello'):
         await message.channel.send('Hello!')
 
-client.run('YOUR_BOT_TOKEN_HERE')
+client.run(token)
+
