81aa4157bfa209d29c4834858bd1f08680306a3e
Tae Yeong Kim <taeyeong200@gmail.com>
2022-12-18 18:54:23 +1100

    fix elo function

--- commands/elo.js
+++ commands/elo.js
@@ -6,8 +6,10 @@ module.exports = {
 		.setDescription('Returns 1v1 and 2v2 elo of user!')
         .addStringOption(option => option.setName('user').setDescription('WB Username').setRequired(true)),
 	async execute(interaction) {
+        await interaction.deferReply();
         user = interaction.options.getString('user');
-        searchlength = (await mvs_client.profiles.search(user)).results.length;
+        search = await mvs_client.profiles.search(user);
+        const searchlength = search.results.length;
         string = "";
         if (searchlength == 1) {
             const user_id = await (await mvs_client.profiles.search(user)).results[0].result.account_id;
@@ -16,8 +18,13 @@ module.exports = {
             console.log(score1v1.score)
             console.log(score2v2.score)
             console.log("1v1: " + score1v1.score + " 2v2: " + score2v2.score)
-            string = string.concat("1v1: " + score1v1.score + " 2v2: " + score2v2.score);
+            string = string.concat("1v1: " + parseInt(score1v1.score) + " 2v2: " + parseInt(score2v2.score));
         }
-        return interaction.reply(string);
+        else {
+            console.log(search)
+            console.log(searchlength)
+            string = string.concat("Multiple users found"); 
+        }
+        await interaction.editReply(string);
 	},
 };
