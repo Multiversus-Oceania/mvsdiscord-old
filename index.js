caa0e2c84a30cd6d09d4838982771e8fa42cb240
Tae Yeong Kim <taeyeong200@gmail.com>
2022-12-19 17:27:37 +1100

    testing

--- index.js
+++ index.js
@@ -32,6 +32,7 @@
 	const mvstoken = await generatemvstoken.getAccessToken();
 	const str_token = JSON.stringify(mvstoken);
 	mvs_client = new MVSClient({accessToken: str_token});
+	console.log("process.env.token: " + process.env.token);
 	console.log("process.env.TOKEN: " + process.env.TOKEN);
-	await client.login(String(process.env.TOKEN));
+	await client.login(process.env.TOKEN);
 })();
