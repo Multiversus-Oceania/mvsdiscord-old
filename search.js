4d53ae2711b3b5b864ffa3c5cd4a32bff4721ffb
Tae Yeong Kim <taeyeong200@gmail.com>
2022-12-20 19:02:13 +1100

    inline

--- search.js
+++ search.js
@@ -102,14 +102,9 @@ async function formatProfile(profile, wbname, user_id, interaction) {
             .setAuthor({name: "taetae"})
             .setDescription('Here are the player stats:')
             .addFields(
-                { name: '1v1', value: `Top char: ${top_1s}\nOverall rank: ${profile.OneVsOne.rank}\nElo: ${parseInt(profile.OneVsOne.score)}` },
-                { name: '2v2', value: `Top char: ${top_2s}\nOverall rank: ${profile.TwoVsTwo.rank}\nElo: ${parseInt(profile.TwoVsTwo.score)}` }
+                { name: '1v1', value: `Top character: ${top_1s}\nOverall rank: ${profile.OneVsOne.rank}\nElo: ${parseInt(profile.OneVsOne.score)}`, inline: true },
+                { name: '2v2', value: `Top character: ${top_2s}\nOverall rank: ${profile.TwoVsTwo.rank}\nElo: ${parseInt(profile.TwoVsTwo.score)}`, inline: true }
             );
-
-// Send the embed object to a Discord channel
-
-
-
         resolve(embed);
     });
 }
