771c8372d3a724ce5561356fc6e2380d8cff0d1e
Tae Yeong Kim <taeyeong200@gmail.com>
2022-12-21 23:24:30 +1100

    check for custom game

--- mvs/maps.js
+++ mvs/maps.js
@@ -1,3 +1,6 @@
 const Maps = {
-    throneRoom: { name: "Throne Room", slug: "map_trophy_room_2_platform"}
+    throneRoom2: { name: "Throne Room", gamemode: "2v2", slug: "map_trophy_room_2_platform"},
+    throneRoom1: { name: "Throne Room", gamemode: "1v1", slug: "map_trophy_room_large_platform"},
+    rooflessScoob2: { name: "Scooby's Haunted Mansion (No Roof)", gamemode: "2v2", slug: "map_scooby_doo_noroof"},
+    batcave2: { name: "Batcave", gamemode: "2v2", slug: "map_batcave"},
 }
