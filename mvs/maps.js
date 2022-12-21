d6c645fcd73645e5d42965192aacf836d0cf068d
Tae Yeong Kim <taeyeong200@gmail.com>
2022-12-22 02:14:08 +1100

    fix player rating bug

--- mvs/maps.js
+++ mvs/maps.js
@@ -4,6 +4,7 @@ const Maps = {
     throneRoom1: { name: "Throne Room", gamemode: "1v1", slug: "map_trophy_room_large_platform"},
     rooflessScoob2: { name: "Scooby's Haunted Mansion (No Roof)", gamemode: "2v2", slug: "map_scooby_doo_noroof"},
     batcave2: { name: "Batcave", gamemode: "2v2", slug: "map_batcave"},
+    skyplat2: { name: "Sky Arena Platforms", gamemode: "2v2", slug: "map_m011"}
 }
 
 
