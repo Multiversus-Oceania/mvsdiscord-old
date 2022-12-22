4d2531902197c51981b32cec0ae62e67efc27456
Tae Yeong Kim <taeyeong200@gmail.com>
2022-12-22 13:26:55 +1100

    testing

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,6 +1,6 @@
 const { REST, Routes } = require('discord.js');
 const fs = require('node:fs');
-require('dotenv').config();
+const path = require('path'); require('dotenv').config({ path: path.join(__dirname, 'mvs.env') });
 
 const commands = [];
 // Grab all the command files from the commands directory you created earlier
